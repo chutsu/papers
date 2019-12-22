a337df9d070d305e2596ec73e3a930b8973a70ba
Chris Choi <chutsu@gmail.com>
2019-12-22 17:18:17 +0000

    Fix index

--- index/scripts/run.sh
+++ index/scripts/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-pdflatex index
-pdflatex index
-bibtex index
-bibtex index
-pdflatex index
-pdflatex index
+# pdflatex index
+# pdflatex index
+# bibtex index
+# bibtex index
+# pdflatex index
+# pdflatex index
